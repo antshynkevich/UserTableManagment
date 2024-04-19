49842d1f54427b5fae1e7254c40a8a65f37d310b
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-19 10:04:52 +0300

    Add AppDbContext and User classes. Configure builder.Services in Program.cs

--- UserTableManagment/Program.cs
+++ UserTableManagment/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using UserTableDataLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<User>(options => 
+{
+    options.Password.RequiredLength = 1;
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
@@ -18,6 +33,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
