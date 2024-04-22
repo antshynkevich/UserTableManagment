64e9186427652c1338fd08d352b95f44d7aadce9
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-22 13:28:16 +0300

    Change a little preset page and views. Add the AccessDenied razor page.

---
+++ UserTableManagment/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserTableManagment.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Method intentionally left empty. It's just an empty page.
+        }
+    }
+}
--- UserTableManagment/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ UserTableManagment/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,19 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using UserTableDataLayer;
 
 namespace UserTableManagment.Areas.Identity.Pages.Account
